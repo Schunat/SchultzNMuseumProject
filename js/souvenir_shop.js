d2e48cc861a509bb5b5f7b4bade6ace5dae46c04
Natalie Schultz <natalie.schultz@bellevuecollege.edu>
2025-08-07 15:14:37 -0700

    saved javascript in js file

--- js/souvenir_shop.js
+++ js/souvenir_shop.js
@@ -1,17 +1,23 @@
 // JavaScript source code
 // Get the modal
 var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("modalImage");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
+var thumbnails = document.getElementsByClassName("souvenir-thumb");
+for (let img of thumbnails) {
+    img.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalImg.alt = this.alt; 
+        modalImg.title = this.title;    
+    } 
 }
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
