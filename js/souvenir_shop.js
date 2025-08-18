be3d43b100d983e3eb0f4fd56642a78c8d582489
Natalie Schultz <natalie.schultz@bellevuecollege.edu>
2025-08-18 08:32:25 -0700

    update Collections html validation image to folder

--- js/souvenir_shop.js
+++ js/souvenir_shop.js
@@ -1,14 +1,14 @@
 // JavaScript source code
 // Get the modal
-var modal = document.getElementById("myModal");
-var modalImg = document.getElementById("modalImage");
+let modal = document.getElementById("myModal");
+let modalImg = document.getElementById("modalImage");
 
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-var thumbnails = document.getElementsByClassName("souvenir-thumb");
+let thumbnails = document.getElementsByClassName("souvenir-thumb");
 for (let img of thumbnails) {
     img.onclick = function () {
         modal.style.display = "block";
